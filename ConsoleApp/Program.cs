27fae2865dd515146e7f290a333191375ba8a9bb
Mukesh <mujoshi1@in.ibm.com>
2023-05-30 17:18:49 +0530

    update code

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,16 @@
-﻿public static class Program
+﻿using static System.Net.Mime.MediaTypeNames;
+
+public static class Program
 {
     public static void Main(string[] args)
     {
         //Console.WriteLine("hello world");
         Console.Write(addNumbers(2, 3));
+        Console.WriteLine("WAITING");
+        Console.WriteLine("Closing in 5 seconds...");
+        System.Threading.Thread.Sleep(2000);
+        System.Environment.Exit(-1);
+        
     }
 
 
